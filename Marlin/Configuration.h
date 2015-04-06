b36ed04b906f191fbbb8a42a62431783f2251442
Daniel Bull <gitlab@neonhorizon.co.uk>
2015-04-06 16:03:57 +0100

    Set stepper directions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -379,8 +379,8 @@ const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR false
-#define INVERT_Z_DIR false
-#define INVERT_E0_DIR false
+#define INVERT_Z_DIR true
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
